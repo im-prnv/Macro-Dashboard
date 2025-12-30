c20dc2355fcf61eea0bcd6df0bf7b0e00562eaa7
im-prnv <132032540+im-prnv@users.noreply.github.com>
2025-12-30 23:52:37 +0530

    Restrict CORS to specific origin

--- backend/main.py
+++ backend/main.py
@@ -38,7 +38,7 @@ app = FastAPI()
 # ---------------- CORS ----------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://im-prnv.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
